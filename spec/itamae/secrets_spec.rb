29ed0d614ed52323bc0a229e369fc624995765c2
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2023-11-25 14:34:08 -0300

    fix: remove unused spec

--- spec/itamae/secrets_spec.rb
+++ spec/itamae/secrets_spec.rb
@@ -4,8 +4,4 @@ describe Itamae::Secrets do
   it 'has a version number' do
     expect(Itamae::Secrets::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
